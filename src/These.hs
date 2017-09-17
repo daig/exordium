be6567003f381dd018afe3fbd80d1bea4c97956d
Dai <dailectic@gmail.com>
2017-09-16 20:36:28 -0400

    Added These

---
+++ src/These.hs
@@ -0,0 +1,14 @@
+module These where
+import Bimap
+
+data These a b = This a | That b | These a b
+instance Bimap These where
+  bimap f g = \case
+    This a -> This (f a)
+    That b -> That (g b)
+    These a b -> These (f a) (g b)
+instance Swap These where
+  swap = \case
+    This a -> That a
+    That b -> This b
+    These a b -> These b a
