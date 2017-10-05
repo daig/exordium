b5768397d81ae6ccb9a45e036352d0e670ad7501
Dai <dailectic@gmail.com>
2017-10-05 16:53:42 -0400

    Fixed Void instances

--- src/Void.hs
+++ src/Void.hs
@@ -7,7 +7,7 @@ import qualified Prelude as P
 __ :: a
 __ = P.undefined
 data X
-instance Plus X where _ + _ = __
-instance Times X where _ * _ = __
-instance Power a X where _ ^ x = x
-instance Power X a where _ ^ _ = __
+instance Plus X where (+) = \case {}
+instance Times X where (*) = \case {}
+instance Power a X where (^) _ = \x -> x
+instance Power X a where (^) = \case {}
