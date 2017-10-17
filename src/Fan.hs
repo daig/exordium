2fe97226e5fe73717436c8734e8c277d6cdae928
Dai G <dogod@sodality.cc>
2017-10-17 07:42:04 -0400

    Fixed tuple families

---
+++ src/Fan.hs
@@ -0,0 +1,16 @@
+module Fan (type (:&:)(..), module X)where
+import Monad as X
+
+data (f :&: g) a = f a :&: g a
+
+instance (Map f, Map g) => Map (f :&: g) where
+  map f (a :&: b) = map f a :&: map f b
+instance (Pure f, Pure g) => Pure (f :&: g) where
+  pure a = (pure a :&: pure a)
+instance (Apply f, Apply g) => Apply (f :&: g) where
+  (f :&: g) |@| (a :&: b) = (f |@| a) :&: (g |@| b)
+-- TODO: this is a bad Bind instance. Does it break laws? Can we do better?
+instance (Bind m, Bind n) => Bind (m :&: n) where
+  bind f (m :&: n) =
+    bind (\x -> case f x of {m' :&: _ -> m'}) m
+    :&: bind (\x -> case f x of {_ :&: n' -> n'}) n
