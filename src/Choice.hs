07d7efc42eaf30704d03ec68b4b2ba5499ae6398
Dai <dailectic@gmail.com>
2017-10-20 17:07:59 -0400

    Added prism method to Choice

--- src/Choice.hs
+++ src/Choice.hs
@@ -1,10 +1,16 @@
 module Choice (Choice(..), (|.), (.|), module X) where
 import Dimap  as X
 import Sum as X (E)
+import Sum
 
 class Dimap p => Choice p where
+  {-# minimal prism | left | right #-}
+  prism :: (b -> t) -> (s -> E t a) -> p a b -> p s t
+  prism constr pat = \p -> dimap pat (either (\x -> x) constr) (right p)
   left :: p a b -> p (E a y) (E b y)
+  left = prism L (either R (\x -> L (R x)))
   right :: p a b -> p (E x a) (E x b)
+  right = \p -> dimap swap swap (left p)
 
 (.|) :: Choice p => p a b -> p (E a y) (E b y)
 (.|) = left
