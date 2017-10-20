d9dee9e42018a352e0aa6c1be296fca50b29192f
Dai <dailectic@gmail.com>
2017-10-20 13:59:08 -0400

    Added natural isomorphisms

--- src/Iso.hs
+++ src/Iso.hs
@@ -1,10 +1,11 @@
 module Iso
-  (type (=~), type (=~~)
-  ,iso
+  (type (=~), type (=~~), type (=:~)
+  ,iso, isoF
   ,module X) where
 import Equality as X
 import Dimap as X
 import Map as X
+import NatTrans as X (type (~>))
 
 type (s =~ a) b t = forall p f. (Dimap p, Map f) => p a (f b) -> p s (f t)
 type (s =~~ a) = forall p f. (Dimap p, Map f) => p a (f a) -> p s (f s)
@@ -14,4 +15,7 @@ iso sa bt = dimap sa (map bt)
 {-# inline iso #-}
 
 
-
+type f =:~ g = forall p h x y. (Dimap p, Map h) => p (g x) (h (g y)) -> p (f x) (h (f y))
+isoF :: f~>g -> g~>f -> f=:~g
+isoF sa bt = dimap sa (map bt)
+{-# inline isoF #-}
--- src/NatTrans.hs
+++ src/NatTrans.hs
@@ -3,3 +3,6 @@ module NatTrans where
 type f ~> g = forall a. f a -> g a
 newtype f ~>. g = NatTrans (f ~> g)
 type (f |~> g) c = forall a. c a => f a -> g a
+
+type p :~> q = forall a b. p a b -> q a b
+newtype p :~>. q = NatTrans2 (p :~> q)
