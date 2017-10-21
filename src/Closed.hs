5f819b9aa146c39c5a3bb956e42e0aa9edc490b5
Dai <dailectic@gmail.com>
2017-10-21 17:02:17 -0400

    Added grate function to Closed

--- src/Closed.hs
+++ src/Closed.hs
@@ -1,7 +1,13 @@
 module Closed (Closed(..), ($.), module X) where
 import Dimap as X
 
-class Dimap p => Closed p where closed :: p a b -> p (x -> a) (x -> b)
+class Dimap p => Closed p where
+  {-# minimal closed | grate #-}
+  closed :: p a b -> p (x -> a) (x -> b)
+  closed = grate (\g x -> g (\f -> f x))
+  grate :: (((s -> a) -> b) -> t) -> p a b -> p s t
+  grate f = \p -> dimap (\a g -> g a) f (closed p)
 
+-- | curry
 ($.) :: Closed p => p (a,b) c -> p a (b -> c)
 ($.) = \p -> (,) `premap` closed p
