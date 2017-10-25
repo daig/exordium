e55c3a7d68d879f939d1b301602f1772866e0118
Dai <dailectic@gmail.com>
2017-10-25 19:06:26 -0400

    Added default pure and extract for adjoints

--- src/Adjoint.hs
+++ src/Adjoint.hs
@@ -1,4 +1,7 @@
-module Adjoint where
+module Adjoint
+  (type (-|)(..)
+  ,pureDefault, extractDefault
+  ,module X) where
 import Monad as X
 import Comonad as X
 import O as X
@@ -17,3 +20,9 @@ class (Monad (f `O` g), Comonad (g `O` f)) => f -| g | f -> g, g -> f where
   leftAdjunct = runStar < view adjoint < Costar
   rightAdjunct :: (a   -> g b) -> f a ->   b
   rightAdjunct = runCostar < review adjoint < Star
+
+pureDefault :: f -| g => a -> O g f a
+pureDefault = O < leftAdjunct id
+
+extractDefault :: f -| g => O f g a -> a
+extractDefault = rightAdjunct id < unO
--- src/O.hs
+++ src/O.hs
@@ -1,8 +1,10 @@
-module O (O(..),module X) where
+module O (O(..),unO,module X) where
 import Pure as X
 import I
 
 newtype O f g a = O (f (g a))
+unO :: O f g a -> f (g a)
+unO (O fga) = fga
 instance (Map f,Map g) => Map (O f g) where map f (O fg) = O (map (map f) fg)
 instance (Pure f,Pure g) => Pure (O f g) where pure a = O (pure (pure a))
 instance (Apply f,Apply g) => Apply (O f g) where O fgf |$| O fga = O (fgf |$(|$|)$| fga)
