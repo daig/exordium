a48b3086b9c11d56d063c8f88570ec20739d9c7f
Dai <dailectic@gmail.com>
2017-10-25 14:35:41 -0400

    Added Forall

---
+++ src/Forall.hs
@@ -0,0 +1,18 @@
+{-# language UndecidableInstances #-}
+{-# language UndecidableSuperClasses #-}
+module Forall (Forall, inst, forall, module X) where
+import Witness as X
+import Coerce
+
+type family Skolem (p :: k -> Constraint) :: k where
+
+class    p (Skolem p) => Forall (p :: k -> Constraint)
+instance p (Skolem p) => Forall (p :: k -> Constraint)
+
+
+inst :: forall p a. Forall p =>. p a
+inst = coerce# (Sub W :: Forall p =>. p (Skolem p))
+
+forall :: forall p. (forall a. W (p a)) -> W (Forall p)
+forall w = case w :: W (p (Skolem p)) of W -> W
+
