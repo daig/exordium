a1510ed1516560a638110c85f15e54e5ffd9fe11
Dai <dailectic@gmail.com>
2018-01-13 17:35:01 -0500

    added Const class

--- src/Arr.hs
+++ src/Arr.hs
@@ -3,13 +3,13 @@ import Dimap as X
 import Category as X
 
 -- dimap f g (arr h) = arr (dimap f g h)
+-- arr f < arr g = arr (f < g)
 -- arr id = id
-class (Dimap p,Category p) => Arr p where
-  {-# minimal arr #-}
+-- arr < const = const
+class (Dimap p,Category p,Const p) => Arr p where
   arr :: (a -> b) -> p a b
-  constP :: b -> p x b
-  constP = \b -> arr (\_ -> b)
 
-instance Arr (->) where
-  arr = id
-  constP = \b _ -> b
+arr_const :: Arr p => r -> p x r
+arr_const r = arr (\_ -> r)
+
+instance Arr (->) where arr = id
