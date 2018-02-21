c0f5b471ed79ab31d000f8e957ac4ec765521be5
Dai <dailectic@gmail.com>
2018-02-21 16:02:49 -0500

    refactor IO.hs

--- src/IO.hs
+++ src/IO.hs
@@ -1,5 +1,5 @@
 {-# language MagicHash #-}
-module IO.Type (module X) where
+module IO (IO(..),module X) where
 import GHC.Types as X (IO(..))
 import Control.Monad.ST as X (RealWorld) -- fix
 import GHC.Prim as X (State#)
