a32beab54e78a8edf9dcecb41a22c2ba6918147e
Dai <dailectic@gmail.com>
2018-03-29 12:28:59 -0400

    doWith

--- src/X/Optic/Do.hs
+++ src/X/Optic/Do.hs
@@ -4,33 +4,37 @@ import X.Arrow.Mapped as X
 import X.Arrow.Folded as X
 import X.Type.K as X
 
-_Do :: Promap p => p (Do f a b) (Do g s t) -> p (a -> f ()) (s -> g ())
+_Do :: Promap p => p (Do f r a b) (Do g  r' s t) -> p (a -> f r) (s -> g r')
 _Do = promap Do runDo
 
-{-doWith :: Map f => (Do (FK f x) a b -> Do (FK f x) s t) -> (a -> f x) -> s -> f ()-}
-{-doWith l afx = case l (Do (\a -> FK (afx a))) of-}
-  {-Do sfkx -> \s -> case sfkx s of FK fx -> constMap () fx-}
+doWith :: (Zero r, Map f) => (Do (FK f x) r a b -> Do (FK f x) r s t) -> (a -> f x) -> s -> f r
+doWith l afx = case l (Do (\a -> FK (afx a))) of
+  Do sfkx -> \s -> case sfkx s of FK fx -> constMap zero fx
 
-newtype Do f a b = Do {runDo :: a -> f ()}
-instance Promap (Do f) where promap f _ (Do b) = Do (premap f b)
-instance Traversed_ (Do f) where lens sa _ (Do x) = Do (\s -> x (sa s))
-instance Pure f => Traversed' (Do f) where
+newtype Do f r a b = Do {runDo :: a -> f r}
+instance Promap (Do f r) where promap f _ (Do b) = Do (premap f b)
+instance Traversed_ (Do f r) where lens sa _ (Do x) = Do (\s -> x (sa s))
+instance (Pure f, Zero r) => Traversed' (Do f r) where
   prism seta _ (Do x) = Do (\s -> case seta s of
-      L _ -> pure ()
+      L _ -> pure zero
       R a -> x a)
-instance Pure f => Traversed0 (Do f) where
+instance (Pure f, Zero r) => Traversed0 (Do f r) where
   lens0 seta _ (Do x) = Do (\s -> case seta s of
-      L _ -> pure ()
+      L _ -> pure zero
       R a -> x a)
-instance Apply f => Traversed1 (Do f) where
-  traversed1 (Do ax) = Do (\ta -> constMap () (traverse1 ax ta))
-instance Applicative f => Traversed (Do f) where
-  traversed (Do ax) = Do (\ta -> constMap () (traverse ax ta))
-instance Folded_ (Do f) where folding_ sa (Do ax) = Do (\s -> ax (sa s))
-instance (Pure f, Zero (f ())) => Folded0 (Do f) where folding0 amsm (Do ax) = Do (\s -> amsm ax s)
-instance (Apply f, Add (f ())) => Folded1 (Do f) where
-  folding1 amsm (Do ax) = Do (\s -> amsm ax s)
-  folded1 (Do ax) = Do (\fa -> foldMap1 ax fa)
-instance Zip f => Closed (Do f) where zipped = mapped
-instance Zip f => Mapped (Do f) where
-  mapped (Do afx) = Do (\za -> constMap () (collect afx za))
+instance (Apply f, Zero r) => Traversed1 (Do f r) where
+  traversed1 (Do ax) = Do (\ta -> constMap zero (traverse1 ax ta))
+instance (Applicative f, Zero r) => Traversed (Do f r) where
+  traversed (Do ax) = Do (\ta -> constMap zero (traverse ax ta))
+instance Folded_ (Do f r) where folding_ sa (Do ax) = Do (\s -> ax (sa s))
+instance (Pure f, Zero r) => Folded0 (Do f r) where
+  folding0 amsm (Do ax) = Do (\s -> unwrapF (amsm (\a -> WrapF (ax a)) s))
+instance (Apply f, Add r, Zero r) => Folded1 (Do f r) where
+  folding1 amsm (Do ax) = Do (\s -> unwrapF (amsm (\a -> WrapF (ax a)) s))
+  folded1 (Do ax) = Do (\fa -> unwrapF (foldMap1 (\a -> WrapF (ax a)) fa))
+instance (Applicative f, Add0 r) => Folded (Do f r) where
+  folding amsm (Do ax) = Do (\s -> unwrapF (amsm (\a -> WrapF (ax a)) s))
+  folded (Do ax) = Do (\fa -> unwrapF (foldMap (\a -> WrapF (ax a)) fa))
+instance (Zip f, Zero r) => Closed (Do f r) where zipped = mapped
+instance (Zip f,Zero r) => Mapped (Do f r) where
+  mapped (Do afx) = Do (\za -> constMap zero (collect afx za))
