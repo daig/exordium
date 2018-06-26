7b3d9b707e82306c672a2d67b980319e5ea8ee07
Dai <dailectic@gmail.com>
2018-06-26 16:52:08 -0400

    add Syntax.Do.X default

---
+++ src/X/Syntax/Do/X.hs
@@ -0,0 +1,25 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+-- | It is best to import this module qualified to avoid ambiguous syntax.
+--
+-- > import qualified X.Syntax.Do as XDo
+--
+-- When imported qualified, the record selectors will work for @-XRecordWildCards@ bindings but won't collide with the syntax itself.
+module X.Syntax.Do.X (DoSyntax(..),doSyntax,module X) where
+import X.Syntax.Do 
+import X.Functor.Monad as X (Monad,Apply,Pure,Map)
+import X.Functor.Monad
+
+-- | Do syntax using our 'Monad' superclasses: 'Map', 'Pure', 'Apply', and 'Bind'.
+-- This means do expressions infer @(Bind m, Pure m)@ rather than @(Monad m)@ whenever possible.
+-- To presuppose the monad laws, add an explicit annotation.
+doSyntax = mkDoSyntax doTuple where
+  doTuple :: ((forall f a b. Map f => (a -> b) -> f a -> f b)
+             ,(forall f a  . Pure f => a -> f a)
+             ,(forall f a b. Apply f => f a -> f b -> f b)
+             ,(forall f a b. Apply f => f (a -> b) -> f a -> f b)
+             ,(forall m a b. Bind m => m a -> (a -> m b) -> m b))
+  doTuple = (map
+            ,pure
+            ,(\fa fb ->  map (\_ b -> b) fa `ap` fb)
+            ,ap
+            ,(\m f -> bind f m))
